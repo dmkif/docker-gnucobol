c83b28973733db1e2a2226f25e66716313a0af4d
da36gubu <daniel.mulzer@fau.de>
2018-09-10 15:17:54 +0200

    fix manifest creation

--- deploy_manifest.sh
+++ deploy_manifest.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 #Login to DockerHub 
 echo "$DOCKER_PASSWORT" | docker login -u "$DOCKER_USER" --password-stdin
-manifestname = "$REPO:$PUBTAG"
-manifeststring = $manifestname
+manifestname="$REPO:$PUBTAG"
+manifeststring=$manifestname
 
 for platform in $ARCH
 do
 #get latest image
 docker pull $REPO:$ARCH-$TRAVIS_BUILD_NUMBER
 #build manifest
-manifeststring = "$manifeststring $REPO:$ARCH-$TRAVIS_BUILD_NUMBER"
+manifeststring="$manifeststring $REPO:$ARCH-$TRAVIS_BUILD_NUMBER"
 done;
 
 docker manifest create $manifeststring
 for platform in $ARCH
 do
 #annotate manifest with correct arch
-docker manifest annotate --arch=$platform --os=linux manifestname $REPO:$ARCH-$TRAVIS_BUILD_NUMBER
+docker manifest annotate --arch=$platform --os=linux $manifestname $REPO:$ARCH-$TRAVIS_BUILD_NUMBER
 done;
 
 docker manifest inspect "${REPO}:${PUBTAG}"
