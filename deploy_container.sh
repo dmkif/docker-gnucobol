e80113859a023c400e1318db9d063eb891b7413a
da36gubu <daniel.mulzer@fau.de>
2018-09-12 09:07:24 +0200

    change to bash

--- deploy_container.sh
+++ deploy_container.sh
@@ -8,9 +8,10 @@ echo "$DOCKER_PASSWORT" | docker login -u "$DOCKER_USER" --password-stdin
 if [ $TRAVIS_BRANCH = "master" ]
  then
    export TAG="latest"
-   docker tag  "$REPO:$ARCH-$TAG" "$REPO:$ARCH-$TRAVIS_BUILD_NUMBER"
-   docker push "$REPO:$ARCH-$TRAVIS_BUILD_NUMBER"
 fi 
+
+docker tag  "$REPO:$ARCH-$TAG" "$REPO:$ARCH-$TRAVIS_BUILD_NUMBER"
+docker push "$REPO:$ARCH-$TRAVIS_BUILD_NUMBER"
 docker push "$REPO:$ARCH-$TAG"
 
 docker logout
